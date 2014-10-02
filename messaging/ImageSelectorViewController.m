2fb2653c62261e6d1ac5c90894d6f91821756428
csilouanos <csilouanos@gmail.com>
2014-10-02 18:28:34 +0300

    Improved image selector. Added the option to snap a live photo.

--- messaging/ImageSelectorViewController.m
+++ messaging/ImageSelectorViewController.m
@@ -10,11 +10,15 @@
 #import "UINavigationBar+Format.h"
 #import "ImageCollectionViewCell.h"
 #import <AssetsLibrary/AssetsLibrary.h>
+#import <MobileCoreServices/MobileCoreServices.h>
 
-@interface ImageSelectorViewController ()
+@interface ImageSelectorViewController ()<UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 
 @property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
 @property (strong, nonatomic) GLPImageSelectorLoader *imageLoader;
+@property (strong, nonatomic) UIImage *snapPhotoImage;
+@property (strong, nonatomic) UIImagePickerController *cameraUI;
+
 
 @end
 
@@ -45,6 +49,8 @@
 
 - (void)initialiseObjects
 {
+    _cameraUI = [[UIImagePickerController alloc] init];
+    _snapPhotoImage = [UIImage imageNamed:@"snap_photo"];
     _imageLoader = [[GLPImageSelectorLoader alloc] init];
     [_imageLoader setDelegate:self];
 }
@@ -65,7 +71,7 @@
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
 {
-    return [_imageLoader numberOfImages];
+    return [_imageLoader numberOfImages] + 1;
 }
 
 
@@ -75,9 +81,14 @@
     
     ImageCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:identifier forIndexPath:indexPath];
     
-//    [cell setDelegate:self];
-    
-    [cell setImageViewImage:[_imageLoader thumbnailAtIndex:indexPath.row]];
+    if(indexPath.row == 0)
+    {
+        [cell setImageViewImage:_snapPhotoImage];
+    }
+    else
+    {
+        [cell setImageViewImage:[_imageLoader thumbnailAtIndex:indexPath.row - 1]];
+    }
     
     return cell;
 }
@@ -86,14 +97,25 @@
 
 - (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
 {
-    dispatch_async(dispatch_get_main_queue(), ^{
-        
-        [_delegate takeImage:[_imageLoader realImageAtIndex:indexPath.row]];
-        
-    });
     
+    if(indexPath.row == 0)
+    {
+        //We are doing that because otherwise the app crashes. iOS 7/8 bug?
+        [self performSelector:@selector(startCameraController) withObject:nil afterDelay:0.3];
+
+    }
+    else
+    {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            
+            [_delegate takeImage:[_imageLoader realImageAtIndex:indexPath.row -1]];
+            
+        });
+        
+        [self.navigationController popViewControllerAnimated:YES];
+
+    }
     
-    [self.navigationController popViewControllerAnimated:YES];
 }
 
 #pragma mark - UICollectionViewDelegateFlowLayout
@@ -116,6 +138,76 @@
 - (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout insetForSectionAtIndex:(NSInteger)section
 {
     return UIEdgeInsetsMake(5.0, 5.0, 5.0, 5.0);
+}
+
+#pragma mark - Camera management
+
+- (BOOL) startCameraController
+{
+    if (([UIImagePickerController isSourceTypeAvailable: UIImagePickerControllerSourceTypeCamera] == NO)
+        || (self == nil)
+        || (self == nil))
+        return NO;
+    
+    
+//    UIImagePickerController *cameraUI = [[UIImagePickerController alloc] init];
+    _cameraUI.sourceType = UIImagePickerControllerSourceTypeCamera;
+    
+    // Displays a control that allows the user to choose picture or
+    // movie capture, if both are available:
+    _cameraUI.mediaTypes = [[NSArray alloc] initWithObjects: (NSString *) kUTTypeImage, nil];
+
+    
+    // Hides the controls for moving & scaling pictures, or for
+    // trimming movies. To instead show the controls, use YES.
+    _cameraUI.allowsEditing = NO;
+    
+    _cameraUI.delegate = self;
+    
+    [self presentViewController:_cameraUI animated:YES completion:nil];
+    
+
+    return YES;
+}
+
+#pragma mark - UIImagePickerControllerDelegate
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    NSString *mediaType = [info objectForKey: UIImagePickerControllerMediaType];
+    UIImage *originalImage, *editedImage, *imageToSave;
+    
+    // Handle a still image capture
+    if (CFStringCompare ((CFStringRef) mediaType, kUTTypeImage, 0)
+        == kCFCompareEqualTo) {
+        
+        editedImage = (UIImage *) [info objectForKey:
+                                   UIImagePickerControllerEditedImage];
+        originalImage = (UIImage *) [info objectForKey:
+                                     UIImagePickerControllerOriginalImage];
+        
+        if (editedImage) {
+            imageToSave = editedImage;
+        } else {
+            imageToSave = originalImage;
+        }
+        
+        [_delegate takeImage: imageToSave];
+        
+        // Save the new image (original or edited) to the Camera Roll
+        UIImageWriteToSavedPhotosAlbum (imageToSave, nil, nil , nil);
+    }
+    
+    [_cameraUI dismissViewControllerAnimated:YES completion:nil];
+
+    
+    [self.navigationController popViewControllerAnimated:YES];
+
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [_cameraUI dismissViewControllerAnimated:YES completion:nil];
 }
 
 - (void)didReceiveMemoryWarning
