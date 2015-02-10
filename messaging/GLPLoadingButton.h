ef6fe9d3551bda537648cc02708a176dae0691ce
csilouanos <csilouanos@gmail.com>
2015-02-10 16:08:13 +0200

    Added the GLPLoadingButton implementation.

---
+++ messaging/GLPLoadingButton.h
@@ -0,0 +1,16 @@
+//
+//  GLPLoadingButton.h
+//  Gleepost
+//
+//  Created by Silouanos on 03/02/15.
+//  Copyright (c) 2015 Gleepost. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GLPLoadingButton : UIButton
+
+- (void)startLoading;
+- (void)stopLoading;
+
+@end
