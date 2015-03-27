9abc8ef93a53f5fcdd892e80d2eed0e7a912f874
csilouanos <csilouanos@gmail.com>
2015-03-27 18:27:01 +0200

    Added a missing intro tagline in the intro view.

---
+++ messaging/GLPVerificationViewController.h
@@ -0,0 +1,13 @@
+//
+//  GLPVerificationViewController.h
+//  Gleepost
+//
+//  Created by Silouanos on 27/03/15.
+//  Copyright (c) 2015 Gleepost. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GLPVerificationViewController : UIViewController
+
+@end
---
+++ messaging/GLPVerificationViewController.m
@@ -0,0 +1,19 @@
+//
+//  GLPVerificationViewController.m
+//  Gleepost
+//
+//  Created by Silouanos on 27/03/15.
+//  Copyright (c) 2015 Gleepost. All rights reserved.
+//
+
+#import "GLPVerificationViewController.h"
+
+@interface GLPVerificationViewController ()
+
+
+
+@end
+
+@implementation GLPVerificationViewController
+
+@end
